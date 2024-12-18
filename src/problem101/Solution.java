f770e2d0d7435ea004772ee83267ff6bfa6f143d
zach <ke3n1@proton.me>
2024-12-18 16:09:02 -0500

    more

---
+++ src/problem101/Solution.java
@@ -0,0 +1,23 @@
+package problem101;
+
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root){
+        //if the list is null return
+        //otherwise run the helper and split the tree into the respective left and right subtrees
+        return root == null || isSymmetricHelper(root.left, root.right);
+    }
+
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right){
+        //if either node is null, return the result of the object comparison
+        if(left == null || right == null){
+            return left == right;
+        }
+        //if the values don't match, the tree is not symmetric
+        if(left.val != right.val){
+            return false;
+        }
+        //need to perform Symmetric checks on subtrees
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+}
