3bd974c0e9709931d48ea8ec2ecbf66e4ff0591f
zach <ke3n1@proton.me>
2024-12-19 18:22:41 -0500

    more

--- src/problem189/Solution.java
+++ src/problem189/Solution.java
@@ -12,13 +12,20 @@ public class Solution {
     }
 
     public void rotateInPlace(int[] nums, int k){
-        int temp;
-        for(int i = 0; i < nums.length; i++){
-            int newIndex = (i + k) % nums.length;
-            temp = nums[newIndex];
-            nums[newIndex] = nums[i];
-            nums[i] = temp;
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
 
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
