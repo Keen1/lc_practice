6f10c82f4f62561903dea123833d93847c571c0a
zach <ke3n1@proton.me>
2025-01-01 13:33:02 -0500

    more

--- src/leet/problem55/Solution.java
+++ src/leet/problem55/Solution.java
@@ -1,30 +1,26 @@
 package leet.problem55;
 /*
-* You are given an integer array nums.
-* You are initially positioned at the array's first index, and each element in the array
-* represents your maximum jump length at that position.
-* Return true if you can reach the last index, or false otherwise
-*
-* example
-* imput nums = {2,3,1,1,4}
-* output: true
-* Jump 1 step from index 0 to 1, then 3 steps to the last index.
-* */
-class Solution {
-    public boolean canJump(int[] nums){
-        int lengthToJump = nums.length - 1;
+You are given an integer array nums. You are initially positioned at the array's first index,
+and each element in the array represents your maximum jump length at that position.
 
-        if(nums[0] - lengthToJump >= 0){
+Return true if you can reach the last index, or false otherwise.
+*/
+
+public class Solution {
+
+    boolean canJump(int[] nums){
+
+        int goal = nums.length;
+        int jumpPower = 0;
+        if(jumpPower == goal){
             return true;
         }
-        int k = 0;
-        int jumps = 0;
-        while(k <= nums.length - 1){
-            jumps = nums[k];
+
+        while(jumpPower < goal){
+
+            jumpPower += nums[jumpPower];
+
 
         }
-        return true;
-
-
     }
 }
